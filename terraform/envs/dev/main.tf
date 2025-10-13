06fa2bf2b8eace1982cd2736211fd8407010902b
NellyAnne <72662380+NellyAnne@users.noreply.github.com>
2025-10-13 13:53:23 -0400

    Create main.tf

---
+++ terraform/envs/dev/main.tf
@@ -0,0 +1,19 @@
+terraform{
+  required_version = ">=1.6.0"
+  required_providers {
+    aws = { source = "hashicorp/aws", version ="~>5.0"}
+  }
+}
+
+module "jobs" {
+SOURCE = "../../modules/glue_jobs"
+for each = var.script_s3_map      # job_name -> script zip
+
+job_name = each.key
+role_arn = var.glue_role_arn
+script_s3 = each.value
+glue_version = "4.0"
+worker_type = "G.1X"
+number_of_workers = 2
+
+}
