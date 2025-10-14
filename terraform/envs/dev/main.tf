662c9e603e0f1bb01217f4a8b2867397205153fc
NellyAnne <72662380+NellyAnne@users.noreply.github.com>
2025-10-14 09:38:12 -0400

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -1,19 +1,18 @@
 terraform{
   required_version = ">=1.6.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version ="~>5.0"}
+    aws = { source = "hashicorp/aws", version = "~> 5.0" }
   }
 }
 
 module "jobs" {
-source = "../../modules/glue_jobs"
-for each = var.script_s3_map      # job_name -> script zip
+  source = "../../modules/glue_jobs"
+  for_each = var.script_s3_map      # job_name -> s3://...zip
 
-job_name = each.key
-role_arn = var.glue_role_arn
-script_s3 = each.value
-glue_version = "4.0"
-worker_type = "G.1X"
-number_of_workers = 2
-
+  job_name = each.key
+  role_arn = var.glue_role_arn
+  script_s3 = each.value
+  glue_version = "4.0"
+  worker_type = "G.1X"
+  number_of_workers = 2
 }
