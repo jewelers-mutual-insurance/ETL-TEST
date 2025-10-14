714774e2620cfcad380a45b65ccc8467201e5ba9
NellyAnne <72662380+NellyAnne@users.noreply.github.com>
2025-10-14 09:18:16 -0400

    Update main.tf

--- terraform/envs/dev/main.tf
+++ terraform/envs/dev/main.tf
@@ -6,7 +6,7 @@ terraform{
 }
 
 module "jobs" {
-SOURCE = "terraform/modules/glue_jobs"
+source = "terraform/modules/glue_jobs"
 for each = var.script_s3_map      # job_name -> script zip
 
 job_name = each.key
